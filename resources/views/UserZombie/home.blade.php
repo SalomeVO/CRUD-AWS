672d11732aab7aa382a6679dad04346bc939cb63
SaloméVO <mvasquezo10@miumg.edu.gt>
2023-10-03 02:10:15 -0600

    se agrega boton temporal para ver la DB

---
+++ resources/views/UserZombie/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.base') <!--para heredar de base-->
+@section('title', 'Agregar') <!--nombre pagina, nombre de seccion-->
+@section('content')
+
+<a class="btn btn-outline-wander btn-xs col-md-12" href="{{ url('/home') }}">
+    <i class="fas fa-database fa-2x"></i>
+</a>
+
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('UserZombie/viewZombie');
+    return view('UserZombie/home');
 });
 
 //Visualizar la lista de usuario
